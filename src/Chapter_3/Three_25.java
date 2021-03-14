4303a000e798f0f691f919a5ae71d8e6971ad308
Soul <61930606+soulsoul233@users.noreply.github.com>
2021-03-14 21:45:36 +0800

    DD

---
+++ src/Chapter_3/Three_25.java
@@ -0,0 +1,13 @@
+package Chapter_3;
+
+import java.util.Scanner;
+
+public class Three_25 {
+    public static Scanner in = new Scanner(System.in);
+    public static void main(String[] args){
+        System.out.println("");
+        double c = in.nextDouble();
+        System.out.println(String.format(" " ));
+
+    }
+}
