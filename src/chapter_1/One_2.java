dcd5ecdbeb9bc7711efc5809a8b37685c34bd14b
Soul <61930606+soulsoul233@users.noreply.github.com>
2021-04-18 19:16:53 +0800

    aaa

--- src/chapter_1/One_2.java
+++
@@ -1,9 +0,0 @@
-package chapter_1;
-
-public class One_2 {
-    public static void main(String[] args){
-        for(int i=1; i<=5; i++){
-            System.out.println("Welcome to Java");
-        }
-    }
-}
