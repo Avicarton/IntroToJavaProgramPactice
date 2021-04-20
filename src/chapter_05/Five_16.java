36cafc559c80674a5b9759980058cac0f78d1d79
Soul <61930606+soulsoul233@users.noreply.github.com>
2021-04-20 10:50:09 +0800

    16

--- src/chapter_05/Five_16.java
+++ src/chapter_05/Five_16.java
@@ -4,11 +4,20 @@ import java.util.Scanner;
 
 public class Five_16 {
     public static Scanner in = new Scanner(System.in);
-    public static void main(String[] args){
-        System.out.print("");
+    public static void main(String[] args) {
+        System.out.print("Enter a number: ");
+        int n = in.nextInt();
+        int p = 2;
 
-        double a = in.nextDouble();
-        
+        while(n > p){
+            if(n % p == 0){
+                n /= p;
+                System.out.print(p + " ");
+            }else {
+                p++;
+            }
+        }
+        System.out.println(n);
 
     }
 }
