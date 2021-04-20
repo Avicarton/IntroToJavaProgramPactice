518e22c9860c26b265c6e3571db3df44b7b15efc
Soul <61930606+soulsoul233@users.noreply.github.com>
2021-04-20 15:00:58 +0800

    18

--- src/chapter_05/Five_18.java
+++ src/chapter_05/Five_18.java
@@ -5,10 +5,50 @@ import java.util.Scanner;
 public class Five_18 {
     public static Scanner in = new Scanner(System.in);
     public static void main(String[] args){
-        System.out.print("");
+        System.out.println("### 1");
+        for(int i=1 ;i<=6;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+        System.out.println();
 
-        double a = in.nextDouble();
-        
+
+        System.out.println("### 2");
+        for(int i=6 ;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+
+        System.out.println("### 3");
+        for(int i=1 ;i<=6;i++){
+            for(int j=1;j<=6 - i;j++){
+                System.out.print(" "+" ");
+            }
+            for(int j=i;j>=1;j--){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+
+        System.out.println("### 4");
+        for(int i=6 ;i>=1;i--){
+            for(int j=1;j<=6 - i;j++){
+                System.out.print(" "+" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+        System.out.println();
 
     }
 }
