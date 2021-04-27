e1210fae9780756ee6a45eb8b6e71428a7ea5f7f
Soul <61930606+soulsoul233@users.noreply.github.com>
2021-04-27 19:02:53 +0800

    six

---
+++ src/chapter_06/Six_07.java
@@ -0,0 +1,14 @@
+package chapter_06;
+
+import java.util.Scanner;
+
+public class Six_07 {
+    public static Scanner in = new Scanner(System.in);
+    public static void main(String[] args){
+        System.out.print("");
+
+        double a = in.nextDouble();
+
+
+    }
+}
